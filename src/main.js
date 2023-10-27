7d3507815aa5a68a62a58a39c779913e152c7cf8
Li_MF <mf-boy@qq.com>
2023-10-27 17:06:45 +0800

    写博客模块

--- src/main.js
+++ src/main.js
@@ -5,8 +5,12 @@ import store from "./store"
 // import '@/static/css/index.scss' // global css
 import ElementUI from "element-ui"
 import "element-ui/lib/theme-chalk/index.css"
+import mavonEditor from "mavon-editor"
+import "mavon-editor/dist/css/index.css"
 
 Vue.use(ElementUI)
+// use
+Vue.use(mavonEditor)
 
 Vue.config.productionTip = false
 
