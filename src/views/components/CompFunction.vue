ae84ab033f726d8441a3eaca9bf47924a9d60d87
Li_MF <mf-boy@qq.com>
2024-10-29 17:24:23 +0800

    在首页新加了 【新增、修改、删除】按钮

---
+++ src/views/components/CompFunction.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="comp_function">
+    <!-- 功能卡片 -->
+    <div class="comp_function-items">
+      <!-- 具名插槽 -->
+      <slot name="write-btn"> </slot>
+    </div>
+  </div>
+</template>
+<style lang="less">
+.comp_function {
+  .comp_function-items {
+    width: 100%;
+    height: 46px;
+    margin: 0 10px;
+    // background-color: #ffffff;
+    border-radius: 6px;
+    // box-shadow: 3px 5px 5px #656b79;
+    position: static;
+    .function_btn {
+      margin: 2px 10px 2px 10px;
+    }
+  }
+}
+</style>
