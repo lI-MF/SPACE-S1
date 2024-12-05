fc7a694573cc2aae9c33686ec199e3bace9ca441
lI-MF <mf-boy@qq.com>
2024-12-05 20:48:13 +0800

    2024/12/5

--- src/views/MyFile/index.vue
+++ src/views/MyFile/index.vue
@@ -3,7 +3,12 @@
   <div class="main-file">
     <div class="main_file-items">
       <!-- 总布局 -->
-       这是归档模块
+       <!-- 标题 -->
+        <h1 class="file_title">归档</h1>
+        <!-- 内容 -->
+         <div class="file_content">
+          <!--  -->
+         </div>
     </div>
   </div>
 </template>
@@ -17,3 +22,27 @@ export default {
   },
 }
 </script>
+<style lang="less" >
+.main-file{
+  position: relative;
+  .main_file-items{
+    // 
+    position: absolute;
+    width: 60%;
+    height: 300px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, 50%);
+    border-radius: 10px 10px 10px 10px;
+    background-color: #ffffff;
+    box-shadow: 0 0 24px rgba(26, 25, 25, 0.2);
+    // 标题
+    .file_title{
+      top: 10px;
+    }
+    .file_content{
+      // 
+    }
+  }
+}
+</style>
